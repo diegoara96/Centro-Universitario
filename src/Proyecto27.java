23152d10cad2d0441ae653d59382d7b43d2edbf1
diegoara96 <diegoaraujonovoa@gmail.com>
2018-01-03 21:32:03 +0100

    anadida nuestro nombre para la autoria en el javadoc

--- src/Proyecto27.java
+++ src/Proyecto27.java
@@ -1,7 +1,12 @@
 
 import java.io.IOException;
 import proyecto.ArranqueBaseDatos;
-
+/**
+ * Proyecto de PII.
+ * Manejo de un centro universitario
+ * @author Diego Araujo, Nerea Airado
+ *
+ */
 public class Proyecto27 {
 
 	public static void main(String[] args) throws IOException {
